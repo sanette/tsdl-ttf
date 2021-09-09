9fb75e71ea8c04ac62ca98ffca03678e934697fe
sanette <sanette-linux@laposte.net>
2021-09-09 10:40:53 +0200

    add docs

---
+++ make_doc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -ve
+
+dune build @doc
+rsync -avz --delete _build/default/_doc/_html/tsdl-ttf/Tsdl_ttf/ docs
+for file in "docs/index.html" "docs/Ttf/index.html"
+do
+  sed -i "s|../../||g" $file
+  sed -i "s|<span>&#45;&gt;</span>|<span class=\"arrow\">→</span>|g" $file
+done
+
+sed -i "s| (tsdl-ttf.Tsdl-Ttf)||g" docs/index.html
+cp ./_build/default/_doc/_html/odoc.css docs/
+cp _build/default/_doc/_html/highlight.pack.js docs/
+echo "header nav {display: none;} header nav.toc {display: block;} header dl dd, header dl dt {display: inline-block;} " >>  docs/odoc.css
+
+echo "Done"
