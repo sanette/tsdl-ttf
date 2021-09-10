26d7111a6b37b675c77e4b60c7533b8245825cbc
sanette <sanette-linux@laposte.net>
2021-09-10 17:25:51 +0200

    can run test without display

--- test/show_string.ml
+++ test/show_string.ml
@@ -10,32 +10,35 @@ let unwind ~(protect:'a -> unit) f x =
   with e -> protect x; raise e
 
 let () =
-  Sdl.init Sdl.Init.everything >>= fun () ->
+  ignore (Sdl.init Sdl.Init.everything);
   Ttf.init () >>= fun () ->
   assert (Ttf.was_init ());
   Ttf.open_font "f500.ttf" 72 >>= fun (font) ->
+  let fg_color = Sdl.Color.create ~r:255 ~g:255 ~b:255 ~a:255 in
+  Ttf.render_text_solid font "foobar" fg_color >>= fun (sface) ->
   let display_width = 640 in
   let display_height = 480 in
-  Sdl.create_window_and_renderer ~w:display_width ~h:display_height Sdl.Window.windowed
-  >>= fun (window, renderer) ->
-  Sdl.render_set_logical_size renderer display_width display_height >>= fun () ->
-  Sdl.get_window_surface window >>= fun display ->
-  let e = Sdl.Event.create () in
-  let r = Sdl.Rect.create ~x:0 ~y:0 ~w:0 ~h:0 in
-  let fg_color = Sdl.Color.create ~r:255 ~g:255 ~b:255 ~a:255 in
-  let rec loop () =
-    Sdl.fill_rect display None 0l >>= fun () ->
-    Ttf.render_text_solid font "foobar" fg_color >>= fun (sface) ->
-    Sdl.blit_surface ~src:sface None ~dst:display (Some r) >>= fun () ->
-    Sdl.update_window_surface window >>= fun () ->
-    match Sdl.wait_event (Some e) with
-    | Error (`Msg err) -> Sdl.log "Could not wait event: %s" err; ()
-    | Ok () ->
-      match Sdl.Event.(enum (get e typ)) with
-      | `Quit | `Key_down -> ()
-      | _ -> loop ()
-  in
-  unwind ~protect:(fun () ->
-      Sdl.destroy_window window;
-      Ttf.quit ();
-      Sdl.quit ()) loop ()
+  match Sdl.create_window_and_renderer ~w:display_width ~h:display_height
+          Sdl.Window.windowed with
+  | Error (`Msg e) -> (Printf.printf "Surface was generated but cannot be shown: \
+                                      %s" e)
+    | Ok (window, renderer) ->
+      Sdl.render_set_logical_size renderer display_width display_height >>= fun () ->
+      Sdl.get_window_surface window >>= fun display ->
+      let e = Sdl.Event.create () in
+      let r = Sdl.Rect.create ~x:0 ~y:0 ~w:0 ~h:0 in
+      let rec loop () =
+        Sdl.fill_rect display None 0l >>= fun () ->
+        Sdl.blit_surface ~src:sface None ~dst:display (Some r) >>= fun () ->
+        Sdl.update_window_surface window >>= fun () ->
+        match Sdl.wait_event (Some e) with
+        | Error (`Msg err) -> Sdl.log "Could not wait event: %s" err; ()
+        | Ok () ->
+          match Sdl.Event.(enum (get e typ)) with
+          | `Quit | `Key_down -> ()
+          | _ -> loop ()
+      in
+      unwind ~protect:(fun () ->
+          Sdl.destroy_window window;
+          Ttf.quit ();
+          Sdl.quit ()) loop ()
