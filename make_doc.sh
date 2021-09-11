8fcef42f3565cd7dd93627ca8e282ec99f276fdb
sanette <sanette-linux@laposte.net>
2021-09-11 08:52:03 +0200

    update doc

--- make_doc.sh
+++ make_doc.sh
@@ -8,9 +8,10 @@ do
   sed -i "s|<span>&#45;&gt;</span>|<span class=\"arrow\">→</span>|g" $file
 done
 
-sed -i "s| (tsdl-ttf.Tsdl-Ttf)||g" docs/index.html
+sed -i "s| (tsdl-ttf.Tsdl_ttf)||g" docs/index.html
 cp ./_build/default/_doc/_html/odoc.css docs/
 cp _build/default/_doc/_html/highlight.pack.js docs/
+chmod 644 docs/odoc.css
 echo "header nav {display: none;} header nav.toc {display: block;} header dl dd, header dl dt {display: inline-block;} " >>  docs/odoc.css
 
 echo "Done"
