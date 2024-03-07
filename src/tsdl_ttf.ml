80139656f550b8fbaf0c58c99166ded7b28a0ba8
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2024-03-06 17:48:23 -0800

    Support LIBSDL2_TTF_SHLIB
    The environment variable avoids the runtime search for the SDL2_ttf shared library and also the Unix-specific fallback to pkg-config.
    
    It allows a debug library (ex. SDL2_ttfd.dll) to be used.

--- src/tsdl_ttf.ml
+++ src/tsdl_ttf.ml
@@ -65,7 +65,7 @@ module Ttf = struct
      #require "tsdl-ttf"
      in the toplevel, see
      https://github.com/ocamllabs/ocaml-ctypes/issues/70 *)
-  let from : Dl.library option =
+  let from_with_search : Dl.library option =
     (if debug then
        Sdl.(
          log_info Log.category_system "Loading Sdl_ttf, Target = %s"
@@ -115,6 +115,11 @@ module Ttf = struct
             print_endline
               ("Cannot find " ^ filename ^ ", please set LIBSDL2_PATH");
             None)
+
+  let from : Dl.library option =
+    let shlib = try Sys.getenv "LIBSDL2_TTF_SHLIB" with Not_found -> "" in
+    if shlib = "" then from_with_search
+    else Some Dl.(dlopen ~filename:shlib ~flags:[ RTLD_NOW ])
 
   let foreign = foreign ?from
   let init = foreign "TTF_Init" (void @-> returning zero_to_ok)
