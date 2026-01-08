49d0f73cdfb836eda9e9f27210276e9e6531d64a
Vu Ngoc San <san.vu-ngoc@laposte.net>
2026-01-08 10:05:41 +0100

    try new window workflow

--- config/discover.ml
+++ config/discover.ml
@@ -14,5 +14,16 @@ let () =
             | None -> default
             | Some deps -> deps)
       in
+      let cflags = if List.mem "-lmingw32" conf.cflags
+          (* Hack to add "-link" before "-mwindows" if it's not already
+             there: *)
+        then let rec loop acc link = function
+            | [] -> List.rev acc
+            | "-mwindows" :: rest when not link ->
+              List.rev_append acc (("-link") :: ("-mwindows") :: rest)
+            | "-link" :: rest -> loop ("-link" :: acc) true rest
+            | a :: rest -> loop (a :: acc) false rest in
+          loop [] false conf.cflags
+        else conf.cflags in
       C.Flags.write_sexp "c_library_flags.sexp" conf.libs;
-      C.Flags.write_sexp "c_flags.sexp" conf.cflags)
+      C.Flags.write_sexp "c_flags.sexp" cflags)
